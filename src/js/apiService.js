59c42a5aeb77579fd8e05b718b00f55c0cda31db
yuriykolisnyk <ykolisnyk@gmail.com>
2021-11-02 16:51:10 +0200

    add apiService

---
+++ src/js/apiService.js
@@ -0,0 +1,32 @@
+export default class ImageApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async fetchImages() {
+    const API_KEY = '24151327-c06d8001d7554d28ff7decb04';
+    const BASE_URL = 'https://pixabay.com/api';
+
+    const response = await fetch(
+      `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`,
+    );
+
+    const newImages = await response.json();
+
+    this.page += 1;
+    return newImages.hits;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
--- src/js/imageFinder.js
+++ src/js/imageFinder.js
@@ -1,2 +1,71 @@
 import ImageApiService from './apiService.js';
 import photoCard from '../templates/imageCard.hbs';
+
+const formSearch = document.querySelector('#search-form');
+const articlesContainer = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('[data-action="load-more"]');
+const noResultMessage = document.querySelector('.no-result');
+
+formSearch.addEventListener('submit', onSearch);
+loadMoreBtn.addEventListener('click', onLoadMore);
+
+const imageApiService = new ImageApiService();
+
+async function onSearch(event) {
+  event.preventDefault();
+
+  try {
+    noResultMessage.classList.add('is-hidden');
+    loadMoreBtn.classList.add('is-hidden');
+
+    const inputSearchValue = event.currentTarget.elements.query.value;
+    imageApiService.query = inputSearchValue;
+
+    imageApiService.resetPage();
+    clearArticlesContainer();
+    const response = await imageApiService.fetchImages();
+
+    if (response.length === 0) {
+      noResultMessage.classList.remove('is-hidden');
+    } else if (response.length > 0) {
+      appendArticlesMarkup(response);
+      loadMoreBtn.classList.remove('is-hidden');
+    }
+    if (response.length < 12) {
+      loadMoreBtn.classList.add('is-hidden');
+    }
+  } catch (error) {
+    console.log('Error');
+  }
+}
+
+async function onLoadMore() {
+  try {
+    const response = await imageApiService.fetchImages();
+    console.log(response);
+
+    appendArticlesMarkup(response);
+    scrollToElement();
+  } catch (error) {
+    console.log('Error');
+  }
+}
+
+function appendArticlesMarkup(articles) {
+  articlesContainer.insertAdjacentHTML('beforeend', photoCard(articles));
+}
+
+function clearArticlesContainer() {
+  articlesContainer.innerHTML = '';
+}
+
+function scrollToElement() {
+  const indexToScroll = 12 * (imageApiService.page - 1) - 11;
+  const itemToScroll = articlesContainer.children[indexToScroll];
+  const options = {
+    speed: 500,
+    verticalOffset: -10,
+  };
+
+  animateScrollTo(itemToScroll, options);
+}
