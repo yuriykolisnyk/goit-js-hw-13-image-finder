e5adf142ff7468773cd73840670dd47653ea5dfc
yuriykolisnyk <ykolisnyk@gmail.com>
2021-11-03 16:12:51 +0200

    add scroll

--- src/js/imageFinder.js
+++ src/js/imageFinder.js
@@ -1,10 +1,14 @@
 import ImageApiService from './apiService.js';
-import photoCard from '../templates/imageCard.hbs';
+import imageCard from '../templates/imageCard.hbs';
+
+import { info, error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
 
 const formSearch = document.querySelector('#search-form');
 const articlesContainer = document.querySelector('.gallery');
+const input = document.querySelector('.input');
 const loadMoreBtn = document.querySelector('[data-action="load-more"]');
-const noResultMessage = document.querySelector('.no-result');
 
 formSearch.addEventListener('submit', onSearch);
 loadMoreBtn.addEventListener('click', onLoadMore);
@@ -14,8 +18,18 @@ const imageApiService = new ImageApiService();
 async function onSearch(event) {
   event.preventDefault();
 
+  if (input.value.trim() === '') {
+    info({
+      text: 'Please try again!',
+      delay: 2000,
+      closerHover: true,
+    });
+    clearArticlesContainer();
+    loadMoreBtn.classList.add('is-hidden');
+    return;
+  }
+
   try {
-    noResultMessage.classList.add('is-hidden');
     loadMoreBtn.classList.add('is-hidden');
 
     const inputSearchValue = event.currentTarget.elements.query.value;
@@ -26,7 +40,11 @@ async function onSearch(event) {
     const response = await imageApiService.fetchImages();
 
     if (response.length === 0) {
-      noResultMessage.classList.remove('is-hidden');
+      error({
+        text: 'No matches found!',
+        delay: 2000,
+        closerHover: true,
+      });
     } else if (response.length > 0) {
       appendArticlesMarkup(response);
       loadMoreBtn.classList.remove('is-hidden');
@@ -45,27 +63,23 @@ async function onLoadMore() {
     console.log(response);
 
     appendArticlesMarkup(response);
-    scrollToElement();
+    btnScrollElem();
   } catch (error) {
     console.log('Error');
   }
 }
 
 function appendArticlesMarkup(articles) {
-  articlesContainer.insertAdjacentHTML('beforeend', photoCard(articles));
+  articlesContainer.insertAdjacentHTML('beforeend', imageCard(articles));
 }
 
 function clearArticlesContainer() {
   articlesContainer.innerHTML = '';
 }
 
-function scrollToElement() {
-  const indexToScroll = 12 * (imageApiService.page - 1) - 11;
-  const itemToScroll = articlesContainer.children[indexToScroll];
-  const options = {
-    speed: 500,
-    verticalOffset: -10,
-  };
-
-  animateScrollTo(itemToScroll, options);
+function btnScrollElem() {
+  loadMoreBtn.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+  });
 }
